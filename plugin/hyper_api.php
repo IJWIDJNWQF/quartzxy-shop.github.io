e6f3773fb5b7ad39675929b9944506a0886aecd2
IJWIDJNWQF <93601777+IJWIDJNWQF@users.noreply.github.com>
2023-06-05 17:41:12 +0700

    Revert "Added new files"
    This reverts commit 6e0af9411d0500d0add40174cd790fb48eb63a9a.

--- plugin/hyper_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once dirname(__FILE__).'/config.php';
-    include_once dirname(__FILE__).'/hyper_class.php';
-
-    $hyper = (object) array(
-        "user" => new User,
-        "connect" => mysqli_connect($db_cofig['db_host'],$db_cofig['db_user'],$db_cofig['db_pass'],$db_cofig['db_name']),
-    );
-?>
